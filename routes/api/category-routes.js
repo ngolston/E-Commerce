5ea22fab6e3b5243151a5ed884cb5c9331c51a4e
Nick G <nick2123@gmail.com>
2021-12-17 21:07:25 -0800

    working on my api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,14 +37,55 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new category
+  try {
+    const categoryDate = await Category.create(req.body);
+    res.status(200).json(categoryDate);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: HTMLTableRowElement.params.id,
+      },
+    }
+  )
+    .then((updateCategory) => {
+      if (!updateCategory) {
+        res.status(404).json({ message: "No category found with that id!" });
+        return;
+      }
+      res.json(updateCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
